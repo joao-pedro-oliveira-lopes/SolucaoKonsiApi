d1e883044fe0e4672b8830137f3fb0871617881c
joao-pedro-oliveira-lopes <154565674+joao-pedro-oliveira-lopes@users.noreply.github.com>
2024-01-15 21:44:44 -0300

    correção

--- KonsiApi/Program.cs
+++ KonsiApi/Program.cs
@@ -1,13 +1,79 @@
+using KonsiApi.Data;
+using KonsiApi.Models;
+using KonsiApi.Repositories;
+using KonsiApi.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Serilog;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.Configure<JwtConfig>(builder.Configuration.GetSection("JwtConfig"));
+builder.Services.Configure<ExternalApiConfig>(builder.Configuration.GetSection("ExternalApiConfig"));
+builder.Services.AddHttpClient<BeneficioService>();
+builder.Services.AddSingleton<RabbitMQService>();
+builder.Services.AddSingleton<CacheService>();
+builder.Services.AddDbContext<DesafioKonsiContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DesafioKonsiDb")));
+builder.Services.AddScoped<ICpfRepository, CpfRepository>();
+var rabbitMQConfig = builder.Configuration.GetSection("RabbitMQConfig").Get<RabbitMQConfig>();
+var beneficiosConsultaQueueName = rabbitMQConfig.Queues["BeneficiosConsulta"];
+builder.Services.AddSingleton<ElasticsearchService>();
+
+
+
+builder.Services.AddTransient<CpfQueueService>(serviceProvider =>
+{
+    var cpfRepository = serviceProvider.GetRequiredService<ICpfRepository>();
+    var rabbitMQService = serviceProvider.GetRequiredService<RabbitMQService>();
+    return new CpfQueueService(cpfRepository, rabbitMQService, beneficiosConsultaQueueName);
+});
+
+var jwtConfig = builder.Configuration.GetSection("JwtConfig");
+var secret = jwtConfig["Secret"];
+var key = Encoding.ASCII.GetBytes(secret);
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.PostgreSQL(builder.Configuration.GetConnectionString("DesafioKonsiDb"),
+                        tableName: "logs")
+    .CreateLogger();
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Host.UseSerilog();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddHostedService<RabbitMQBackgroundService>();
 var app = builder.Build();
+
+// Inicializar e enfileirar CPFs
+using (var scope = app.Services.CreateScope())
+{
+    var cpfQueueService = scope.ServiceProvider.GetRequiredService<CpfQueueService>();
+    await cpfQueueService.EnqueueCpfsAsync();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
